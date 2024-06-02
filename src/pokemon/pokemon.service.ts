b5ca419b639c4e9b9d31c17e0d4f3931c20eb712
Deschain <jorgeadrian.kadasoftware@gmail.com>
2024-06-01 19:23:56 -0600

    Version 1.0.0

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -4,14 +4,21 @@ import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { Model, isValidObjectId } from 'mongoose';
 import { Pokemon } from './entities/pokemon.entity';
 import { InjectModel } from '@nestjs/mongoose';
+import { PaginationDto } from '../common/dto/pagination.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PokemonService {
 
+  private defaultLimit: number;
+
   constructor(
     @InjectModel(Pokemon.name)
-    private readonly pokemonModel:Model<Pokemon>
-  ){}
+    private readonly pokemonModel:Model<Pokemon>,
+    private readonly configService: ConfigService,
+  ){
+    this.defaultLimit = configService.get<number>('defaultLimit')
+  }
 
   async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLowerCase();
@@ -24,8 +31,17 @@ export class PokemonService {
     }
   }
 
-  findAll() {
-    return `This action returns all pokemon`;
+  findAll(paginationDto: PaginationDto) {
+
+    const {limit = 10, offset = 0} = paginationDto;
+
+    return this.pokemonModel.find()
+    .limit(limit)
+    .skip(offset)
+    .sort({
+      no:1 // Ordenar la columna no de manera ascendente
+    })
+    .select('-__v') // Restarle a la respuesta JSON la propiedad "__v"
   }
 
   async findOne(term: string) {
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,24 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import axios, {AxiosInstance} from 'axios';
 import { PokeResponse } from './interface/poke-response.iterface';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
-
-  private readonly axios: AxiosInstance;
-
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+    private readonly http:AxiosAdapter,
+  ){}
+  
   async executeSeed(){
-    const {data} = await axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10')
-    
-    data.results.forEach(({name,url}) => {
 
+    await this.pokemonModel.deleteMany({}); // delete * from pokemons;
+
+    const data:PokeResponse = await this.http.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650')
+    const pokemonToInsert: {name:string, no:number}[] = []
+
+    data.results.forEach(async({name,url}) => {
       const segments = url.split('/')
       const no: number = +segments[segments.length - 2]
-      
-      console.log({name, no})
-
+      // const pokemon = await this.pokemonModel.create({name, no});
+      pokemonToInsert.push({name, no});
     })
-    return data.results;
+
+    await this.pokemonModel.insertMany(pokemonToInsert)
+
+    return 'Seed executed succesfully';
   }
 
 }
